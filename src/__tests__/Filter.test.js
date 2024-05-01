dc4b8088efb4e358f582078a570bfd8ee313b749
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-05-01 03:31:28 +0300

    filter.test modifications

--- src/__tests__/Filter.test.js
+++ src/__tests__/Filter.test.js
@@ -2,14 +2,16 @@ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Filter from "../components/Filter";
 
+const categories = ["All", "Produce", "Dairy", "Dessert"]; // Define categories for testing
+
 test("displays the <select> element", () => {
-  render(<Filter />);
+  render(<Filter categories={categories} />); // Pass categories prop
   expect(screen.queryByRole("combobox")).toBeInTheDocument();
 });
 
 test("calls the onCategoryChange callback prop when the <select> is changed", () => {
   const onCategoryChange = jest.fn();
-  render(<Filter onCategoryChange={onCategoryChange} />);
+  render(<Filter categories={categories} onCategoryChange={onCategoryChange} />); // Pass categories prop
 
   fireEvent.change(screen.queryByRole("combobox"), {
     target: { value: "Dairy" },
